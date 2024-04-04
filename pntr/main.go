9be6b140b3403f977d8135d820e1dcf2908ead22
zuki <zuki@home>
2024-04-04 17:32:20 +0700

    edited pntr

--- pntr/main.go
+++ pntr/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func doubleNumber(number *int) {
+	*number = *number * 2
+}
+
 func main() {
 	fmt.Println("Pointer test")
+	num := 12
+	doubleNumber(&num)
+	fmt.Println("After doubleNumber(), num:", num)
 }
