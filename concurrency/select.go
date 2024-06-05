117a27a505f2202fbe6affe56659c13b06917cf9
zuki <zukidono@gmail.com>
2024-06-05 21:53:18 +0700

    select is finished

--- concurrency/select.go
+++ concurrency/select.go
@@ -2,55 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-func GenerateRandomInt(min, max int) int {
-	// Use NewSource to create a local random source
-	source := rand.NewSource(time.Now().UnixNano())
-	generator := rand.New(source)
-	// Generate a random number between min (inclusive) and max (exclusive)
-	return generator.Intn(max-min) + min
-}
-
-func GenerateRandomString(length int) string {
-	// Use NewSource to create a local random source
-	source := rand.NewSource(time.Now().UnixNano())
-	generator := rand.New(source)
-	b := make([]byte, length)
-	for i := range b {
-		// Generate a random index within the charset string
-		index := generator.Intn(len(charset))
-		// Assign the character at the random index to the slice
-		b[i] = charset[index]
-	}
-	return string(b)
-}
-
-func generateRandom(string_chan chan string, int_chan chan int) {
-	randomString := GenerateRandomString(10)
-	randomInt := GenerateRandomInt(0, 100)
-
+func sleep_t(signal_chan chan int, sec time.Duration) {
+	signal := 0
 	for {
 		select {
-		case string_chan <- randomString:
-			randomString = GenerateRandomString(10)
-		case int_chan <- randomInt:
-			randomInt = GenerateRandomInt(0, 100)
+		case signal = <-signal_chan:
+			if signal == 69 {
+				return
+			}
+		default:
+			fmt.Println("I'm sleeping")
+			time.Sleep(sec * time.Second)
 		}
 	}
 }
 
 func selectGo() {
-	string_chan := make(chan string)
-	int_chan := make(chan int)
+	signal_chan := make(chan int)
 
-	go func() {
-		fmt.Println(<-string_chan)
-		fmt.Println(<-int_chan)
-	}()
-	generateRandom(string_chan, int_chan)
+	go sleep_t(signal_chan, 10)
+
+	time.Sleep(30 * time.Second)
+	signal_chan <- 69
 }
