f93a0e590f811c495b1614ffdeb2e5bb2064b84b
zuki <zukidono@gmail.com>
2024-06-06 22:56:11 +0700

    added mutex

---
+++ mutex/go.mod
@@ -0,0 +1,3 @@
+module github.com/zukigit/work-go/mutex
+
+go 1.22.3
---
+++ mutex/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var v = make(map[string]int)
+var mu sync.Mutex
+
+func adddOne() {
+	mu.Lock()
+	v["test"]++
+	mu.Unlock()
+}
+
+func main() {
+	fmt.Println("It's mutex")
+
+	for i := 0; i < 100; i++ {
+		go adddOne()
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("v['test']", v["test"])
+}
