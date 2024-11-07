cf1f264d7f1e16e90f0a341224d7aa4682d13cf4
zuki <zukidono@gmail.com>
2024-11-07 20:51:46 +0700

    change log format

--- logging/main.go
+++ logging/main.go
@@ -11,22 +11,37 @@ import (
 func main() {
 	fmt.Println("it's loggin!")
 
-	logger_config := zap.NewProductionConfig()
-	logger_config.Encoding = "console"
-	logger_config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("20060102150405")
-	logger_config.OutputPaths = []string{
-		"/tmp/test.log",
-		"stdout",
+	loggerConfig := zap.NewProductionConfig()
+	loggerConfig.Encoding = "console" // Use console encoding
+	loggerConfig.EncoderConfig = zapcore.EncoderConfig{
+		MessageKey:       "msg",
+		ConsoleSeparator: " ", // Tab-separated fields
 	}
-	logger, err := logger_config.Build()
+
+	// Output logs to both console and a file
+	loggerConfig.OutputPaths = []string{
+		"/tmp/test.log", // Log to a file
+		"stdout",        // Log to the console
+	}
+
+	// Build the logger
+	logger, err := loggerConfig.Build()
 	if err != nil {
 		fmt.Println("ERROR:", err)
+		return
 	}
-	defer logger.Sync() // flushes buffer, if any
+	defer logger.Sync() // Flushes buffer, if any
 
-	// sugar := logger.Sugar().With(
-	// 	zap.String("ProgramName", "logging"),
-	// )
+	// Create a SugaredLogger
 	sugar := logger.Sugar()
-	sugar.Infof("Failed to fetch URL: %s, attempt %d, backoff: %s", "localhost", 3, time.Second)
+
+	// Log an example message
+	pid := 344
+	threads := 2
+	sugar.Infof("[%s] [INFO] [%d] [%d] %s",
+		time.Now().Format("20060102150405.000"),
+		pid,     // PID
+		threads, // Threads
+		"Failed to fetch URL: localhost, attempt 3, backoff: 1s", // Log message
+	)
 }
